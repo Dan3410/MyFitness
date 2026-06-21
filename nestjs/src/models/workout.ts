6fba4363c1ff4beb6bd1629c104b84008519d80a
Daniel Salvia <danmats140@gmail.com>
2026-06-21 16:57:24 -0300

    feat: modified workout class on nestjs. Added endpoints to nestjs

--- nestjs/src/models/workout.ts
+++ nestjs/src/models/workout.ts
@@ -1,50 +1,85 @@
 export interface WorkoutListItem {
-    id: number
-    name: string
-    category: string
-    estimatedTime: number
-    lastTimeDone: Date
-    createdDate: Date
-    modifiedDate: Date
+    id: number;
+    name: string;
+    category: string;
+    estimatedTime: number;
+    lastTimeDone: Date;
+    createdDate: Date;
+    modifiedDate: Date;
 }
 
-export interface Workout{
-    id: number
-    name: string
-    category: string
+export type StepType =
+    | 'REST'
+    | 'WARMUP'
+    | 'COOLDOWN'
+    | 'SET'
+    | 'GYM_EXERCISE'
+    | 'GYM_INTERVAL'
+    | 'SWIM_DISTANCE'
+    | 'SWIM_TIME'
+    | 'RUN_DISTANCE'
+    | 'RUN_TIME'
+    | 'RUN_CALORIES';
 
+export type SwimGear = 'FINS' | 'PULLBUOY' | 'PADDLES' | 'SNORKEL';
+export type SwimStroke =
+    | 'IM'
+    | 'FREESTYLE'
+    | 'BACKSTROKE'
+    | 'BREASTSTROKE'
+    | 'BUTTERFLY'
+    | 'CHOICE';
+
+export interface BaseStep {
+    type: StepType;
 }
 
-export interface set{
-    repeat: number
-
-    steps: Array<swimStep | runStep | gymStep>
+export interface RestStep extends BaseStep {
+    type: 'REST';
+    seconds: number;
 }
 
-export interface swimStep{
-    //Rest, swim distance or swim time
-    type: string
-    
-    distance: number | null
-
-    time: number | null
-
-    //Can make a type of this
-    gear: Array<string>
+export interface WorkoutSet extends BaseStep {
+    type: 'SET';
+    repeat: number;
+    steps: WorkoutStep[];
 }
 
-export interface runStep{
-    //Rest, run distance, run time or calories
-    type: string
-    
-    distance: number | null
-
-    time: number | null
-
+export interface SwimStep extends BaseStep {
+    type: 'SWIM_DISTANCE' | 'SWIM_TIME';
+    distance: number | null;
+    time: number | null;
+    gear: SwimGear[];
+    stroke: SwimStroke;
 }
 
-export interface gymStep{
-    exercise: string
-    
-    repsOrTime: number
+export interface RunStep extends BaseStep {
+    type: 'RUN_DISTANCE' | 'RUN_TIME' | 'RUN_CALORIES';
+    distance: number;
+    calories: number;
+    time: number | null;
+    speed: number | null;
+}
+
+export interface GymStep extends BaseStep {
+    type: 'GYM_EXERCISE' | 'GYM_INTERVAL';
+    exercise: string;
+    byTime: boolean;
+    reps: number;
+    time: number;
+    weight: number;
+}
+
+export type WorkoutStep =
+    | RestStep
+    | WorkoutSet
+    | SwimStep
+    | RunStep
+    | GymStep;
+
+export interface Workout {
+    id: string;
+    name: string;
+    category: string;
+    steps: WorkoutStep[];
 }
--- nestjs/src/modules/workout/workout.service.ts
+++ nestjs/src/modules/workout/workout.service.ts
@@ -1,32 +1,59 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
-import { WorkoutListItem } from 'src/models/workout';
+import { Workout, WorkoutListItem } from 'src/models/workout';
 import { WorkoutCategories } from 'src/models/workoutCategories';
 
 @Injectable()
 export class WorkoutService {
+  private workoutApiUrl = 'http://localhost:8080/workout';
 
-  private workoutApiUrl = "http://localhost:8080/workout";
-  constructor(private readonly httpService: HttpService){}
+  constructor(private readonly httpService: HttpService) {}
 
   async getCategories(): Promise<WorkoutCategories[]> {
-    const response = await firstValueFrom(this.httpService.get(this.workoutApiUrl + "/categories"));
+    const response = await firstValueFrom(
+      this.httpService.get<WorkoutCategories[]>(`${this.workoutApiUrl}/categories`),
+    );
     return response.data;
   }
 
-    async getWorkoutSteps(id: string): Promise<WorkoutListItem[]> {
-    const response = (await firstValueFrom(this.httpService.get(this.workoutApiUrl + "/steps/" + id)));
-    return response.data
-  }
-  
-  async getWorkouts(id: string, category: string): Promise<WorkoutListItem[]> {
-    const response = (await firstValueFrom(this.httpService.get(this.workoutApiUrl + "/list/" + id + `?category=${category}`)));
-    return response.data
+  async getWorkoutSteps(id: string): Promise<WorkoutListItem[]> {
+    const response = await firstValueFrom(
+      this.httpService.get<WorkoutListItem[]>(`${this.workoutApiUrl}/steps/${id}`),
+    );
+    return response.data;
   }
 
-  async deleteWorkout(id: string): Promise<WorkoutListItem[]>{
-    const response = (await firstValueFrom(this.httpService.delete(this.workoutApiUrl + `/${id}`)));
-    return response.data
+  async getWorkouts(
+    id: string,
+    category: string,
+  ): Promise<WorkoutListItem[]> {
+    const response = await firstValueFrom(
+      this.httpService.get<WorkoutListItem[]>(
+        `${this.workoutApiUrl}/list/${id}?category=${category}`,
+      ),
+    );
+    return response.data;
+  }
+
+  async deleteWorkout(id: string): Promise<WorkoutListItem[]> {
+    const response = await firstValueFrom(
+      this.httpService.delete<WorkoutListItem[]>(`${this.workoutApiUrl}/${id}`),
+    );
+    return response.data;
+  }
+
+  async getWorkout(id: string): Promise<Workout> {
+    const response = await firstValueFrom(
+      this.httpService.get<Workout>(`${this.workoutApiUrl}/${id}`),
+    );
+    return response.data;
+  }
+
+  async editWorkout(id: string, workout: Workout): Promise<Workout> {
+    const response = await firstValueFrom(
+      this.httpService.put<Workout>(`${this.workoutApiUrl}/${id}`, workout),
+    );
+    return response.data;
   }
 }
